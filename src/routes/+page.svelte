dab5fb1fce3c4a637ba62e270d01342cc08e000b
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2024-10-19 21:42:15 +0200

    feat: login page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import Button from "$lib/components/ui/button/button.svelte";
+  import { MenubarMenu, MenubarTrigger, Menubar, MenubarContent, MenubarItem } from "$lib/components/ui/menubar";
 </script>
 
+<Menubar>
+  <MenubarMenu>
+    <MenubarTrigger>File</MenubarTrigger>
+    <MenubarContent>
+      <MenubarItem>1234</MenubarItem>
+    </MenubarContent>
+  </MenubarMenu>
+</Menubar>
+
 <Button>Test</Button>
-
-<style>
-
-</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Login from "$lib/components/form/login.svelte";
+</script>
+
+<div class="container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-2">
+  <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
+    <div class="absolute inset-0 bg-cover background-img bg-center" />
+    <div class="relative z-20 flex items-center text-2xl">
+      Positron
+    </div>
+  </div>
+  <div class="lg:p-8">
+    <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+      <div class="flex flex-col space-y-2 text-center">
+        <h1 class="text-2xl font-semibold tracking-tight">
+          Login
+        </h1>
+        <p class="text-muted-foreground text-sm">
+          Enter your login details below
+        </p>
+      </div>
+      <Login />
+    </div>
+  </div>
+</div>
+
+<style>
+  .background-img {
+    background-image: url($lib/images/login.png?enhanced);
+  }
+</style>
