799646790baefb741cc3a16171982f8ad1feec70
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-03-03 21:44:05 +0100

    style: fixed settings style

--- frontend/src/routes/account/+layout.svelte
+++ frontend/src/routes/account/+layout.svelte
@@ -20,14 +20,14 @@
   ];
 </script>
 
-<div class="space-y-6 p-10">
+<div class="space-y-6 p-10 w-full">
   <div class="space-y-0.5">
     <h2 class="text-2xl font-bold tracking-tight">Settings</h2>
     <p class="text-muted-foreground">Manage your account</p>
   </div>
   <Separator class="my-6" />
   <div class="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-    <aside class="-mx-4 lg:max-w-1/5 lg:w-52">
+    <aside class="lg:max-w-1/5 lg:w-52">
       <Sidebar items={routes} class="" />
     </aside>
     <div class="flex-1">
