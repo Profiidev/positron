31fe9c497168f0d026937f92231d1b6ea6784620
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-05-13 23:46:10 +0200

    fix: wrong static asset path

--- backend/src/well_known.rs
+++ backend/src/well_known.rs
@@ -3,9 +3,6 @@ use serde_json::Value;
 
 pub fn route() -> Vec<Route> {
   rocket::routes![assetlinks]
-    .into_iter()
-    .flat_map(|route| route.map_base(|base| format!("{}{}", "/.well-known", base)))
-    .collect()
 }
 
 pub fn state(server: Rocket<Build>) -> Rocket<Build> {
