a080c8a02ab16fc72c815dc126637ad101ab81e6
GitHub Action <action@github.com>
2025-12-01 21:01:01 +0000

    Fix code style issues with Prettier

--- frontend/src/lib/components/nav/sidebar-app/user.svelte
+++ frontend/src/lib/components/nav/sidebar-app/user.svelte
@@ -26,7 +26,7 @@
   <Sidebar.MenuItem>
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        {#snippet child({ props }: { props: Record<string, any> })}
+        {#snippet child({ props }: { props: Record })}
           <Sidebar.MenuButton
             size="lg"
             class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
