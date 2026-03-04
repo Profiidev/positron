517c4f1857476e3fa5eaf83a28576102264ebddf
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-03-04 15:09:53 +0100

    fix: error impl

--- backend/src/s3/error.rs
+++ backend/src/s3/error.rs
@@ -1,8 +1,15 @@
-use centaurus::impl_from_error;
 use http::StatusCode;
 use thiserror::Error;
 
-impl_from_error!(S3Error, StatusCode::INTERNAL_SERVER_ERROR);
+impl From<S3Error> for centaurus::error::ErrorReport {
+  #[track_caller]
+  fn from(value: S3Error) -> Self {
+    Self {
+      error: centaurus::eyre::Report::new(value),
+      status: StatusCode::INTERNAL_SERVER_ERROR,
+    }
+  }
+}
 
 #[derive(Error, Debug)]
 pub enum S3Error {
