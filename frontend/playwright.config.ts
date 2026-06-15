ea57f2f607584a8b22bcb8e1537d17fba1ff5b6a
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-06-15 11:34:01 +0200

    test: test more e2e browsers

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,6 +1,31 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
+  fullyParallel: true,
+  projects: [
+    /* Test against desktop browsers */
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] }
+    },
+    /* Test against mobile viewports. */
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 8'] }
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] }
+    }
+  ],
   reporter: [
     ['html'],
     ['junit', { outputFile: 'test-results/frontend-e2e.xml' }]
